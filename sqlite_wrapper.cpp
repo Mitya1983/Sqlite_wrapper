ecf184e5184195c000d6a8e15ac429d0a69db0cb
Mitya <tristan.it@outlook.com>
2019-05-17 23:07:22 +0300

    Exceptions code was moved to Sqlite_wrapper.h and .cpp. for portability

--- sqlite_wrapper.cpp
+++ sqlite_wrapper.cpp
@@ -3,7 +3,93 @@
 #include <chrono>
 #include <thread>
 #include <mutex>
+Sqlite3Exception::Sqlite3Exception(const std::string &databaseName, const std::string &details, const std::string &msg)
+{
+    _msg = "Error from SQL on database ";
+    _msg += std::move(databaseName);
+    _msg += ": On ";
+    _msg += std::move(details);
+    _msg += "- ";
+    _msg += std::move(msg);
+}
 
+const char *Sqlite3Exception::what() const noexcept
+{
+    return _msg.c_str();
+}
+
+CreateDatabaseException::CreateDatabaseException(const std::string &msg)
+{
+    _msg = std::move(msg);
+}
+
+const char *CreateDatabaseException::what() const noexcept
+{
+    return _msg.c_str();
+}
+
+TableException::TableException(const std::string &databaseName, const std::string &tableName, const std::string &msg)
+{
+    _msg = "Error on table ";
+    _msg += std::move(tableName);
+    _msg += " of database ";
+    _msg += std::move(databaseName);
+    _msg += ": ";
+    _msg += std::move(msg);
+}
+
+const char *TableException::what() const noexcept
+{
+    return _msg.c_str();
+}
+
+ColumnException::ColumnException(const std::string &databaseName, const std::string &tableName,
+                                 const std::string &columnName, const std::string &msg)
+{
+    _msg = "Error on column ";
+    _msg += std::move(columnName);
+    _msg += " of table ";
+    _msg += std::move(tableName);
+    _msg += " of databse ";
+    _msg += std::move(databaseName);
+    _msg += ": ";
+    _msg += std::move(msg);
+}
+
+const char *ColumnException::what() const noexcept
+{
+    return _msg.c_str();
+}
+
+InsertException::InsertException(const std::string &databaseName, const std::string &tableName, const std::string &msg)
+{
+    _msg = "Error on table ";
+    _msg += std::move(tableName);
+    _msg += " of databse ";
+    _msg += std::move(databaseName);
+    _msg += ": ";
+    _msg += std::move(msg);
+}
+
+const char *InsertException::what() const noexcept
+{
+    return _msg.c_str();
+}
+
+SelectException::SelectException(const std::string &databaseName, const std::string &tableName, const std::string &msg)
+{
+    _msg = "Error on table ";
+    _msg += std::move(tableName);
+    _msg += " of databse ";
+    _msg += std::move(databaseName);
+    _msg += ": ";
+    _msg += std::move(msg);
+}
+
+const char *SelectException::what() const noexcept
+{
+    return _msg.c_str();
+}
 Query Sqlite_wrapper::_result;
 bool Sqlite_wrapper::firstQuery = true;
 int Sqlite_wrapper::callback(void *, int argc, char **argv, char **azColName)
--- sqlite_wrapper.h
+++ sqlite_wrapper.h
@@ -1,13 +1,58 @@
 #ifndef SQLITE_WRAPPER_H
 #define SQLITE_WRAPPER_H
 #include <sqlite3.h>
+#include <exception>
 #include <string>
 #include <queue>
 #include <map>
 #include <vector>
 #include <memory>
 
-#include "exceptions.h"
+class Sqlite3Exception : public std::exception
+{
+    std::string _msg;
+public:
+    Sqlite3Exception(const std::string &databaseName, const std::string &details, const std::string &msg);
+    virtual const char *what() const noexcept;
+};
+
+class CreateDatabaseException : public std::exception
+{
+    std::string _msg;
+public:
+    CreateDatabaseException(const std::string &msg);
+    virtual const char *what() const noexcept;
+};
+class TableException : public std::exception
+{
+    std::string _msg;
+public:
+    TableException(const std::string &databaseName, const std::string &tableName, const std::string &msg);
+    virtual const char *what() const noexcept;
+};
+class ColumnException : public std::exception
+{
+    std::string _msg;
+public:
+    ColumnException(const std::string &databaseName, const std::string &tableName,
+                    const std::string &columnName, const std::string &msg);
+    virtual const char *what() const noexcept;
+};
+class InsertException : std::exception
+{
+    std::string _msg;
+public:
+    InsertException(const std::string &databaseName, const std::string &tableName, const std::string &msg);
+    virtual const char *what() const noexcept;
+};
+class SelectException : public std::exception
+{
+    std::string _msg;
+public:
+    SelectException(const std::string &databaseName, const std::string &tableName, const std::string &msg);
+    virtual const char *what() const noexcept;
+};
+
 
 struct resultColumn
 {
